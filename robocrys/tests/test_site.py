2822a2a0f216ddc4b0c2d840d86b248386e30f0b
Alex Ganose <alexganose@googlemail.com>
2018-11-20 15:29:17 -0800

    TST: Add tests for next nearest neighbour functions

--- robocrys/tests/test_site.py
+++ robocrys/tests/test_site.py
@@ -75,9 +75,41 @@ class TestSiteAnalyzer(unittest.TestCase):
         self.assertEqual(info[0]["sym_id"], 0)
         self.assertAlmostEqual(info[0]["dist"], 1.2619877178483)
 
-    def test_get_next_nearest_neighbor_summary(self):
-        """Check getting nearest neighbor information."""
+    def test_get_next_nearest_neighbor_info(self):
+        """Check getting next nearest neighbor information."""
         sa = SiteAnalyzer(self.tin_dioxide)
-        # desc = sa._get_next_nearest_neighbor_info(0)
-        desc = sa.get_next_nearest_neighbor_summary(0)
-        print(desc)
+        info = sa._get_next_nearest_neighbor_info(0)
+
+        self.assertEqual(len(info), 15)
+        self.assertEqual(info[0]["element"], "O")
+        self.assertEqual(info[0]["connectivity"], "corner-sharing")
+        self.assertAlmostEqual(info[0]["geometry"]["type"], "trigonal planar")
+
+        # check different structure
+        sa = SiteAnalyzer(self.ba_n)
+        info = sa._get_next_nearest_neighbor_info(0)
+        self.assertEqual(len(info), 50)
+        self.assertEqual(info[5]["element"], "N")
+        self.assertEqual(info[5]["connectivity"], "edge-sharing")
+        self.assertAlmostEqual(info[5]["geometry"]["type"], "octahedral")
+
+    def test_get_next_nearest_neighbor_summary(self):
+        """Check getting next nearest neighbor summary for all neighbors."""
+        sa = SiteAnalyzer(self.tin_dioxide)
+        info = sa.get_next_nearest_neighbor_summary(5)
+        self.assertEqual(info["Sn"]["n_sites"], 12)
+        self.assertTrue("corner-sharing" in info["Sn"])
+        self.assertEqual(info["Sn"]['corner-sharing']['n_sites'], 8)
+        self.assertEqual(info["Sn"]['corner-sharing']['geometries'][0],
+                         'octahedral')
+
+    def test_get_nearest_neighbor_summary(self):
+        """Check getting nearest neighbor summary for all neighbors."""
+        sa = SiteAnalyzer(self.tin_dioxide)
+        info = sa.get_nearest_neighbor_summary(5)
+        self.assertTrue("O" in info)
+        self.assertEqual(info["O"]["n_sites"], 6)
+        self.assertEqual(info["O"]['sym_groups'][0]['n_sites'], 1)
+        self.assertEqual(info["O"]['sym_groups'][0]['sym_id'], 2)
+        self.assertAlmostEqual(info["O"]['sym_groups'][0]['dists'][0],
+                               2.051317869078315)
