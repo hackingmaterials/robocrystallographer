0f088394f9618337b70cecf24c8ae9c4b28d78e0
Alex Ganose <alexganose@googlemail.com>
2018-12-03 10:57:23 -0800

    Finish vdw heterostructure finding

--- robocrys/component.py
+++ robocrys/component.py
@@ -1,13 +1,13 @@
 """
 This module implements functions for handling structure components.
 """
+from copy import deepcopy
 
 import numpy as np
 
 from monty.fractions import gcd
 from typing import List, Dict, Text, Any, Tuple
 
-from pymatgen.analysis.chemenv.utils.coordination_geometry_utils import Plane
 from pymatgen.core.structure import Structure, PeriodicSite
 from pymatgen.core.periodic_table import get_el_sp
 from pymatgen.core.composition import Composition, iupac_ordering_dict
@@ -43,6 +43,7 @@ def get_sym_inequiv_components(components: List[Component],
         - ``"inequivalent_ids"`` (``list[int]``): The site indices of the
             symmetry inequivalent atoms in the component.
     """
+    components = deepcopy(components)
     sym_inequiv_components = {}
     equivalent_atoms = spg_analyzer.get_symmetry_dataset()['equivalent_atoms']
 
@@ -98,6 +99,7 @@ def get_formula_inequiv_components(components: List[Component],
             with two Ga2As2 components would be 4.
         - ``"formula"`` (``list[int]``): The reduced formula of the component.
     """
+    components = deepcopy(components)
     inequiv_components = {}
 
     for component in components:
@@ -244,9 +246,9 @@ def get_formula_from_components(components: List[Component],
     return reduced_form
 
 
-def components_are_heterostructure(components: List[Component]
-                                   ) -> bool:
-    """Determines whether a list of components form a heterostructure.
+def components_are_vdw_heterostructure(components: List[Component]
+                                       ) -> bool:
+    """Whether a list of components form a van der Waals heterostructure.
 
     A heterostructure is defined here as a structure with more than one
     formula inequivalent 2D component.
@@ -266,10 +268,10 @@ def components_are_heterostructure(components: List[Component]
         return False
 
 
-def get_heterostructure_information(components: List[Component],
-                                    use_iupac_formula: bool=True,
-                                    ) -> Dict[Text, Any]:
-    """Gets information about the ordering of components in a heterostructure.
+def get_vdw_heterostructure_information(components: List[Component],
+                                        use_iupac_formula: bool=True,
+                                        ) -> Dict[Text, Any]:
+    """Gets information about ordering of components in a vdw heterostructure.
 
     Args:
         components: A list of structure components, generated using
@@ -300,36 +302,57 @@ def get_heterostructure_information(components: List[Component],
         - ``"intercalants"`` (``list[component]``: A :obj:`List` of intercalated
             components.
     """
-    if not components_are_heterostructure(components):
+    if not components_are_vdw_heterostructure(components):
         raise ValueError("Components do not form a heterostructure.")
 
-    millers = [c['orientation'] for c in components if c['dimensionality'] == 2]
+    try:
+        millers = set([c['orientation'] for c in components
+                       if c['dimensionality'] == 2])
+    except KeyError as e:
+        if "orientation" in str(e):
+            raise KeyError("Components not generated with inc_orientation=True")
+        else:
+            raise e
+
     if len(millers) != 1:
         raise ValueError("2D components don't all have the same orientation.")
 
     cart_miller = components[0]['structure'].lattice.get_cartesian_coords(
         millers.pop()).tolist()
 
-    plane = Plane(cart_miller + [0])
+    # plane is used to find the distances of all components along a certain axis
+    # should use normal vector of plane to get exact distances but we just care
+    # about relative distances.
+    def distances_to_plane(points):
+        return [np.dot(cart_miller, pp) for pp in points]
 
-    min_distances = [min(plane.distances(c['structure'].cart_coords))
+    min_distances = [min(distances_to_plane(c['structure'].cart_coords))
                      for c in components]
+
+    # sort the components by distance to plane
     ordering = np.argsort(min_distances)
     ordered_components = [components[x] for x in ordering]
+
+    # only consider the layered components formulae
     ordered_layers = [c for c in ordered_components if c['dimensionality'] == 2]
     ordered_layers_formula = [
         c['structure'].composition.get_reduced_formula_and_factor(
             use_iupac_formula)[0] for c in ordered_layers]
     num_layer_formulas = len(set(ordered_layers_formula))
 
-    repeating_formula = ordered_layers_formula.copy()
+    repeating_formula = ordered_layers_formula
     num_repetitions = 1
-    for n in range(int(np.floor(len(ordered_layers)/num_layer_formulas)),
-                   0, -1):
-        if (len(ordered_layers) % n == 0 and
-                all([len(set(ordered_layers[i::num_layer_formulas])) == 1
-                     for i in range(n)])):
-            repeating_formula = ordered_layers_formula[:len(ordered_layers)/n]
+
+    # depending on the number of inequivalent formulae, there is a maximum
+    # number of repetitions that can occur. To avoid unnecessary work we start
+    # from this number of repetitions and move to 1 repetition (e.g. no
+    # repetition)
+    max_repetitions = int(np.floor(len(ordered_layers)/num_layer_formulas))
+    for n in range(max_repetitions, 0, -1):
+        if (all([len(set(ordered_layers_formula[i::num_layer_formulas])) == 1
+                 for i in range(n)]) and len(ordered_layers) % n == 0):
+            repeating_formula = ordered_layers_formula[
+                                :int(len(ordered_layers)/n)]
             num_repetitions = n
             break
 
