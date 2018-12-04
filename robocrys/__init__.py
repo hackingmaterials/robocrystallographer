91c8b811311b13e7de692d09a59af0054ab83d72
Alex Ganose <alexganose@googlemail.com>
2018-12-03 17:35:14 -0800

    Add formula mappings

--- robocrys/__init__.py
+++ robocrys/__init__.py
@@ -1,3 +1,10 @@
+from monty.serialization import loadfn
+
+from pkg_resources import resource_filename
+
 from robocrys.mineral import MineralMatcher
 from robocrys.site import SiteAnalyzer
 from robocrys.structure import StructureCondenser
+
+
+formula_mapping = loadfn(resource_filename('robocrys', 'formula_db.json.gz'))
