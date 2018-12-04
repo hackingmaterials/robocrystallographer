f774af919d058d3d9be87c1da1dc6cb51c2fdcc3
Alex Ganose <alexganose@googlemail.com>
2018-12-04 12:24:20 -0800

    Use pebble for multiprocessing

--- dev_scripts/download_pubchem.py
+++ dev_scripts/download_pubchem.py
@@ -8,7 +8,7 @@ WARNING: While in progress the disk usage maybe fairly large (several GB).
 """
 
 import ftplib
-import multiprocessing
+import pebble
 import os
 
 import pybel
@@ -16,93 +16,77 @@ import pybel
 from tqdm import tqdm
 from monty.serialization import dumpfn
 
-key_name_map = {'PUBCHEM_IUPAC_OPENEYE_NAME': 'openeye',
-                'PUBCHEM_IUPAC_NAME': 'iupac',
-                'PUBCHEM_IUPAC_CAS_NAME': 'cas',
-                'PUBCHEM_IUPAC_SYSTEMATIC_NAME': 'iupac',
-                'PUBCHEM_IUPAC_TRADITIONAL_NAME': 'traditional'}
-
-n_proc = multiprocessing.cpu_count()
-
-
-def process_sdf_file(filename):
-    skipped = 0
-    smiles_iso_to_can = {}
-    smiles_can_to_name = {}
-
-    for i, mol in enumerate(pybel.readfile('sdf', filename)):
-        try:
-            c_smiles_can = mol.data['PUBCHEM_OPENEYE_CAN_SMILES']
-            c_smiles_iso = mol.data['PUBCHEM_OPENEYE_ISO_SMILES']
-
-            names = {}
-            for key in ['PUBCHEM_IUPAC_CAS_NAME',
-                        'PUBCHEM_IUPAC_NAME',
-                        'PUBCHEM_IUPAC_TRADITIONAL_NAME']:
-                if key in mol.data:
-                    names[key_name_map[key]] = mol.data[key]
-
-            if not names:
-                raise KeyError("name")
-
-            smiles_iso_to_can[c_smiles_iso] = c_smiles_can
-            smiles_can_to_name[c_smiles_can] = names
-
-        except KeyError:
-            # skip if no smiles or no names
-            skipped += 1
-
-    return smiles_iso_to_can, smiles_can_to_name, skipped
-
-
-def download_and_process(filename):
-    ftp = ftplib.FTP('ftp.ncbi.nlm.nih.gov',)
-    ftp.login()
-    ftp.cwd("pubchem/Compound/CURRENT-Full/SDF")
-    ftp.retrbinary("RETR " + filename, open(filename, 'wb').write)
-
-    data = process_sdf_file(filename)
-    os.remove(filename)
-
-    return data
-
+keys = ['PUBCHEM_OPENEYE_CAN_SMILES', 'PUBCHEM_OPENEYE_ISO_SMILES',
+        'PUBCHEM_IUPAC_CAS_NAME', 'PUBCHEM_IUPAC_NAME',
+        'PUBCHEM_IUPAC_TRADITIONAL_NAME', 'PUBCHEM_IUPAC_SYSTEMATIC_NAME',
+        'PUBCHEM_IUPAC_OPENEYE_NAME']
 
 list_ftp = ftplib.FTP('ftp.ncbi.nlm.nih.gov',)
 list_ftp.login()
 
 list_ftp.cwd("pubchem/Compound/CURRENT-Full/SDF")
 files = [f for f in list_ftp.nlst() if '.sdf.gz' in f]
+files = files[:3]
 total_files = len(files)
 
 pbar = tqdm(total=total_files)
 results = []
 
 
-def update(val):
-    results.append(val)
+def process_sdf_file(filename):
+    ftp = ftplib.FTP('ftp.ncbi.nlm.nih.gov',)
+    ftp.login()
+    ftp.cwd("pubchem/Compound/CURRENT-Full/SDF")
+    ftp.retrbinary("RETR " + filename, open(filename, 'wb').write)
+
+    skipped = 0
+    pubchem_molecules = {}
+    for i, mol in enumerate(pybel.readfile('sdf', filename)):
+        try:
+            pubchem_id = mol.data['PUBCHEM_COMPOUND_CID']
+            xyz = mol.write(format="xyz")
+
+            data = {'xyz': xyz}
+            for key in keys:
+                if key in mol.data:
+                    data[key] = mol.data[key]
+
+            pubchem_molecules[pubchem_id] = data
+
+        except KeyError:
+            skipped += 1
+
+    os.remove(filename)
+    return pubchem_molecules, skipped
+
+
+def task_done(future):
+    try:
+        result = future.result()
+        results.append(result)
+    except TimeoutError as e:
+        print("File {} timed-out".format(e.args[1]))
+    except Exception as e:
+        raise e
     pbar.update()
 
 
-pool = multiprocessing.Pool(n_proc)
-for file in files:
-    pool.apply_async(download_and_process, args=(file, ), callback=update)
+with pebble.ProcessPool() as pool:
+    for remote_file_path in files:
+        f = pool.schedule(process_sdf_file, args=(remote_file_path, ),
+                          timeout=60)
+        f.add_done_callback(task_done)
 
-pool.close()
-pool.join()
 pbar.close()
 
-smiles_iso_to_can_full = {}
-smiles_can_to_name_full = {}
+molecules = {}
 total_skipped = 0
 
-for smiles_iso, smiles_can, skip in results:
-    smiles_iso_to_can_full.update(smiles_iso)
-    smiles_can_to_name_full.update(smiles_can)
-    total_skipped += skip
+for r_pubchem_molecules, r_skipped in results:
+    molecules.update(r_pubchem_molecules)
+    total_skipped += r_skipped
 
 print("total skipped: {}".format(total_skipped))
-print("total saved: {}".format(len(smiles_can_to_name_full.keys())))
+print("total saved: {}".format(len(molecules.keys())))
 
-smiles_can_to_name_full['smiles_map'] = smiles_iso_to_can_full
-
-dumpfn(smiles_can_to_name_full, "smiles_to_name.json.gz")
+dumpfn(molecules, "pubchem.json.gz")
