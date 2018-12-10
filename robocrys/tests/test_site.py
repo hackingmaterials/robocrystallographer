69ac9c8ee86bb38adfc2b3a1e828e6b2fe111d3c
Alex Ganose <alexganose@googlemail.com>
2018-12-10 12:39:58 -0800

    TST: Add tests for site inequivalence methods

--- robocrys/tests/test_site.py
+++ robocrys/tests/test_site.py
@@ -1,6 +1,7 @@
 from pymatgen.analysis.local_env import CrystalNN
 
-from robocrys.site import SiteAnalyzer
+from robocrys.site import SiteAnalyzer, geometries_match, nn_summaries_match, \
+    nnn_summaries_match
 from robocrys.util import RobocrysTest
 
 
@@ -49,7 +50,7 @@ class TestSiteAnalyzer(RobocrysTest):
 
         self.assertEqual(len(info), 3)
         self.assertEqual(info[0]["element"], "Sn")
-        self.assertEqual(info[0]["sym_id"], 4)
+        self.assertEqual(info[0]["inequiv_id"], 4)
         self.assertAlmostEqual(info[0]["dist"], 2.0857160137)
 
         # check different structure
@@ -57,7 +58,7 @@ class TestSiteAnalyzer(RobocrysTest):
         info = sa._get_nearest_neighbor_info(0)
         self.assertEqual(len(info), 6)
         self.assertEqual(info[0]["element"], "N")
-        self.assertEqual(info[0]["sym_id"], 0)
+        self.assertEqual(info[0]["inequiv_id"], 0)
         self.assertAlmostEqual(info[0]["dist"], 1.2619877178483)
 
     def test_get_next_nearest_neighbor_info(self):
@@ -92,13 +93,17 @@ class TestSiteAnalyzer(RobocrysTest):
         sa = SiteAnalyzer(self.tin_dioxide)
         info = sa.get_next_nearest_neighbor_summary(5)
         self.assertEqual(info["Sn"]["n_sites"], 12)
-        self.assertTrue("corner-sharing" in info["Sn"])
-        self.assertEqual(info["Sn"]['corner-sharing']['n_sites'], 8)
-        self.assertEqual(info["Sn"]['corner-sharing']['geometries'][0],
-                         'octahedral')
-        self.assertAlmostEqual(info["Sn"]["corner-sharing"]["angles"][0],
-                               130.16984393647132)
-        self.assertEqual(len(info["Sn"]["corner-sharing"]["angles"]), 8)
+        self.assertTrue("corner-sharing" in info["Sn"]['connectivities'])
+        self.assertEqual(
+            info["Sn"]['connectivities']['corner-sharing']['n_sites'], 8)
+        self.assertEqual(
+            info["Sn"]['connectivities']['corner-sharing']['geometries'][0],
+            'octahedral')
+        self.assertAlmostEqual(
+            info["Sn"]['connectivities']["corner-sharing"]["angles"][0],
+            130.16984393647132)
+        self.assertEqual(
+            len(info["Sn"]['connectivities']["corner-sharing"]["angles"]), 8)
 
     def test_get_nearest_neighbor_summary(self):
         """Check getting nearest neighbor summary for all neighbors."""
@@ -106,7 +111,75 @@ class TestSiteAnalyzer(RobocrysTest):
         info = sa.get_nearest_neighbor_summary(5)
         self.assertTrue("O" in info)
         self.assertEqual(info["O"]["n_sites"], 6)
-        self.assertEqual(info["O"]['sym_groups'][0]['n_sites'], 1)
-        self.assertEqual(info["O"]['sym_groups'][0]['sym_id'], 2)
-        self.assertAlmostEqual(info["O"]['sym_groups'][0]['dists'][0],
+        self.assertEqual(info["O"]['inequiv_groups'][0]['n_sites'], 1)
+        self.assertEqual(info["O"]['inequiv_groups'][0]['inequiv_id'], 2)
+        self.assertAlmostEqual(info["O"]['inequiv_groups'][0]['dists'][0],
                                2.051317869078315)
+
+    def test_equivalent_sites(self):
+        """Check equivalent sites instance variable set correctly."""
+        sa = SiteAnalyzer(self.ba_n, use_symmetry=False)
+        self.assertEqual(sa.equivalent_sites, [0, 0, 0, 0, 4, 4])
+
+        # test using symmetry to determine equivalent sites
+        sa = SiteAnalyzer(self.ba_n, use_symmetry=True)
+        self.assertEqual(sa.equivalent_sites.tolist(), [0, 0, 0, 0, 4, 4])
+
+        # test symprec option works
+        sa = SiteAnalyzer(self.ba_n, use_symmetry=True, symprec=0.0001)
+        self.assertEqual(sa.equivalent_sites.tolist(), [0, 1, 1, 0, 4, 4])
+
+    def test_get_inequivalent_site_ids(self):
+        sa = SiteAnalyzer(self.ba_n, use_symmetry=False)
+        inequiv_ids = sa.get_inequivalent_site_ids(list(range(6)))
+        self.assertEqual(inequiv_ids, [0, 4])
+
+        # test using symmetry to determine inequivalent sites.
+        sa = SiteAnalyzer(self.ba_n, use_symmetry=True)
+        inequiv_ids = sa.get_inequivalent_site_ids(list(range(6)))
+        self.assertEqual(inequiv_ids, [0, 4])
+
+        # test symprec option
+        sa = SiteAnalyzer(self.ba_n, use_symmetry=True, symprec=0.0001)
+        inequiv_ids = sa.get_inequivalent_site_ids(list(range(6)))
+        self.assertEqual(inequiv_ids, [0, 1, 4])
+
+    def test_geometries_match(self):
+        """Test geometry matching function."""
+        sa = SiteAnalyzer(self.ba_n, use_symmetry=True)
+        geom_a = sa.get_site_geometry(0)
+        geom_b = sa.get_site_geometry(1)
+        geom_c = sa.get_site_geometry(4)
+
+        self.assertTrue(geometries_match(geom_a, geom_b))
+        self.assertFalse(geometries_match(geom_a, geom_c))
+
+        # test likeness tol works
+        self.assertFalse(geometries_match(geom_a, geom_b, likeness_tol=1e-10))
+
+    def test_nn_summaries_match(self):
+        """Test nearest neighbour summary matching function."""
+        sa = SiteAnalyzer(self.ba_n, use_symmetry=True)
+        nn_a = sa.get_nearest_neighbor_summary(0, split_into_groups=False)
+        nn_b = sa.get_nearest_neighbor_summary(1, split_into_groups=False)
+        nn_c = sa.get_nearest_neighbor_summary(4, split_into_groups=False)
+
+        self.assertTrue(nn_summaries_match(nn_a, nn_b))
+        self.assertFalse(nn_summaries_match(nn_a, nn_c))
+
+        # test bond dist tol works
+        self.assertFalse(nn_summaries_match(nn_a, nn_b, bond_dist_tol=1e-10))
+
+    def test_nnn_summaries_match(self):
+        """Test nearest neighbour summary matching function."""
+        sa = SiteAnalyzer(self.ba_n, use_symmetry=True)
+        nnn_a = sa.get_next_nearest_neighbor_summary(0)
+        nnn_b = sa.get_next_nearest_neighbor_summary(1)
+        nnn_c = sa.get_next_nearest_neighbor_summary(4)
+
+        self.assertTrue(nnn_summaries_match(nnn_a, nnn_b))
+        self.assertFalse(nnn_summaries_match(nnn_a, nnn_c))
+
+        # test bond angle tol works
+        self.assertFalse(nnn_summaries_match(
+            nnn_a, nnn_b, bond_angle_tol=1e-10))
