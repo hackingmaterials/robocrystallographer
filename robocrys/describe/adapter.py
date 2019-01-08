095b4930421018561911c2c48f54298034f3fc99
Alex Ganose <alexganose@googlemail.com>
2019-01-08 15:44:48 -0800

    Update describe adapter to subclass base adapter

--- robocrys/describe/adapter.py
+++ robocrys/describe/adapter.py
@@ -8,6 +8,7 @@ from collections import namedtuple, defaultdict
 from typing import Dict, Any, List, Union
 
 from pymatgen.core.periodic_table import get_el_sp
+from robocrys.adapter import BaseAdapter
 
 ComponentDetails = namedtuple('ComponentDetails',
                               ['formula', 'count', 'dimensionality',
@@ -28,11 +29,10 @@ NextNeighborSiteDetails = namedtuple('NextNeighborSiteDetails',
                                       'poly_formula'])
 
 
-class DescriptionAdapter(object):
+class DescriptionAdapter(BaseAdapter):
     """Class to facilitate pulling data from the condensed structure dictionary.
 
     Attributes:
-        elements: The site elements.
         sym_labels: The symmetry labels as strings.
         use_iupac_ordering (bool, optional): Whether to order formulas
             by the iupac "electronegativity" series, defined in
@@ -49,11 +49,9 @@ class DescriptionAdapter(object):
 
     def __init__(self, condensed_structure: Dict[str, Any],
                  use_iupac_ordering: bool = True):
-        self._condensed_structure = condensed_structure
-        self.use_iupac_ordering = use_iupac_ordering
+        super().__init__(condensed_structure)
 
-        self.elements = {site_index: site_data['element']
-                         for site_index, site_data in self.sites.items()}
+        self.use_iupac_ordering = use_iupac_ordering
         self.sym_labels = {site_index: self.get_sym_label(site_index)
                            for site_index in self.sites.keys()}
 
@@ -176,44 +174,6 @@ class DescriptionAdapter(object):
 
         return sorted(nnn_details, key=self._site_order)
 
-    def get_distance_details(self, from_site: int,
-                             to_sites: Union[int, List[int]]) -> List[float]:
-        """Gets the bond lengths between two sets of sites.
-
-        Args:
-            from_site: An inequivalent site index.
-            to_sites: One ore more inequivalent site indices.
-
-        Returns:
-            The distances between the sites.
-        """
-        if isinstance(to_sites, int):
-            # If only one to_site is provided turn it into a list
-            to_sites = [to_sites]
-
-        return [distance for to_site in to_sites
-                for distance in self.distances[from_site][to_site]]
-
-    def get_angle_details(self, from_site: int, to_sites: Union[int, List[int]],
-                          connectivity: str) -> List[float]:
-        """Gets the connectivity angles between two sets of sites.
-
-        Args:
-            from_site: An inequivalent site index.
-            to_sites: One ore more inequivalent site indices.
-            connectivity: The site connectivity type. I.e. "corner", "edge", or
-                "face".
-
-        Returns:
-            The distances between the sites.
-        """
-        if isinstance(to_sites, int):
-            # If only one to_site is provided turn it into a list
-            to_sites = [to_sites]
-
-        return [angle for to_site in to_sites
-                for angle in self.angles[from_site][to_site][connectivity]]
-
     def get_component_details(self) -> List[ComponentDetails]:
         """Gets a summary of all components.
 
@@ -291,7 +251,7 @@ class DescriptionAdapter(object):
             - ``element`` (``str``): The site element.
             - ``sites`` (``list[int]``): A list of site indices in this group.
         """
-        sites = self.components[component_index]['sites']
+        sites = list(set(self.components[component_index]['sites']))
 
         grouped_sites = defaultdict(list)
         for site_index in sites:
@@ -324,96 +284,6 @@ class DescriptionAdapter(object):
         all_labels = sorted([label for site_index in site_indices for label in
                              self.sites[site_index]['sym_labels']])
         return "({})".format(",".join(map(str, sorted(all_labels))))
-
-    @property
-    def mineral(self) -> Dict[str, Union[str, int, bool]]:
-        """The mineral data.
-
-        See :meth:`robocrys.condense.StructureCondenser.condense_structure` for
-        more details.
-        """
-        return self._condensed_structure['mineral']
-
-    @property
-    def formula(self) -> str:
-        """The structure formula.
-
-        See :meth:`robocrys.condense.StructureCondenser.condense_structure` for
-        more details.
-        """
-        return self._condensed_structure['formula']
-
-    @property
-    def spg_symbol(self) -> str:
-        """The space group symbol.
-
-        See :meth:`robocrys.condense.StructureCondenser.condense_structure` for
-        more details.
-        """
-        return self._condensed_structure['spg_symbol']
-
-    @property
-    def crystal_system(self) -> str:
-        """The crystal system.
-
-        See :meth:`robocrys.condense.StructureCondenser.condense_structure` for
-        more details.
-        """
-        return self._condensed_structure['crystal_system']
-
-    @property
-    def dimensionality(self) -> int:
-        """The overall dimensionality.
-
-        See :meth:`robocrys.condense.StructureCondenser.condense_structure` for
-        more details.
-        """
-        return self._condensed_structure['dimensionality']
-
-    @property
-    def sites(self) -> Dict[int, Dict[str, Any]]:
-        """The site data.
-
-        See :meth:`robocrys.condense.StructureCondenser.condense_structure` for
-        more details.
-        """
-        return self._condensed_structure['sites']
-
-    @property
-    def distances(self) -> Dict[int, Dict[int, List[float]]]:
-        """The distance data.
-
-        See :meth:`robocrys.condense.StructureCondenser.condense_structure` for
-        more details.
-        """
-        return self._condensed_structure['distances']
-
-    @property
-    def angles(self) -> Dict[int, Dict[int, Dict[str, List[float]]]]:
-        """The angle data.
-
-        See :meth:`robocrys.condense.StructureCondenser.condense_structure` for
-        more details.
-        """
-        return self._condensed_structure['angles']
-
-    @property
-    def components(self) -> Dict[int, Dict[str, Any]]:
-        """The component data.
-
-        See :meth:`robocrys.condense.StructureCondenser.condense_structure` for
-        more details.
-        """
-        return self._condensed_structure['components']
-
-    @property
-    def component_makeup(self) -> List[int]:
-        """The component makeup of the structure.
-
-        See :meth:`robocrys.condense.StructureCondenser.condense_structure` for
-        more details.
-        """
-        return self._condensed_structure['component_makeup']
 
     def _site_order(self, s: Union[SiteGroup,
                                    NeighborSiteDetails,
