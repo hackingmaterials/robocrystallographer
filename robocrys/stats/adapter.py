33663420f1fe7ccec8d01bea9d51334345bef8e5
Alex Ganose <alexganose@googlemail.com>
2019-01-09 18:23:11 -0800

    Stats updates

--- robocrys/stats/adapter.py
+++ robocrys/stats/adapter.py
@@ -3,6 +3,7 @@ This module implements a class to resolve the symbolic references in condensed
 structure data.
 """
 from collections import Counter, defaultdict
+from statistics import mean
 
 from typing import Dict, Any
 
@@ -13,6 +14,9 @@ from robocrys.util import defaultdict_to_dict
 
 class StatisticsAdapter(BaseAdapter):
     """Class to facilitate calculating statistics from condensed structures.
+
+    Attributes:
+        species: A list of species present in the structure.
 
     Args:
         condensed_structure: The condensed structure data, formatted as produced
@@ -28,6 +32,10 @@ class StatisticsAdapter(BaseAdapter):
         if not inc_oxi_state:
             self.elements = {site_index: get_el_sp(element).name
                              for site_index, element in self.elements.items()}
+        self.species = list(set(self.elements.values()))
+        self.component_species = {
+            c_index: list(set([self.elements[s] for s in c_data['sites']]))
+            for c_index, c_data in self.components.items()}
 
     def get_site_connectivities(self):
         """Get the connectivity types for each site.
@@ -80,8 +88,7 @@ class StatisticsAdapter(BaseAdapter):
         poly_sites = [site for component_index in self.component_makeup
                       for site in self.components[component_index]['sites']
                       if self.sites[site]['poly_formula']]
-        poly_elements = [self.elements[site] for site in poly_sites
-                         if self.sites[site]['poly_formula']]
+        poly_elements = [self.elements[site] for site in poly_sites]
         n_elements = self.get_element_count()
 
         counts = defaultdict(lambda: defaultdict(int))
@@ -92,3 +99,33 @@ class StatisticsAdapter(BaseAdapter):
                 counts[element][connectivity] += count
 
         return defaultdict_to_dict(counts)
+
+    def get_element_corner_sharing_octahedra_tilt_angles(
+            self) -> Dict[str, Dict[str, int]]:
+        """Gets the average tilt angles for corner sharing octahedral sites.
+
+        Returns:
+            The average tilt angles for each element in the structure, if
+            that element is found in a corner sharing octahedral geometry.
+            Formatted as::
+
+                {element: tilt_angle}
+        """
+
+        oct_sites = [site for component_index in self.component_makeup
+                     for site in self.components[component_index]['sites']
+                     if self.sites[site]['geometry']['type'] == 'octahedral' and
+                     'corner' in self.sites[site]['nnn']]
+
+        el_angles = defaultdict(list)
+        for site in oct_sites:
+            nnn_sites = [
+                nnn_site for nnn_site in self.sites[site]['nnn']['corner']
+                if self.sites[nnn_site]['geometry']['type'] == 'octahedral']
+            angles = [abs(180 - angle) for nnn_site in nnn_sites for
+                      angle in self.angles[site][nnn_site]['corner']]
+            if angles:
+                el_angles[self.elements[site]].extend(angles)
+
+        return {el: mean(angles) for el, angles in el_angles.items()}
+
--- robocrys/stats/structure.py
+++ robocrys/stats/structure.py
@@ -4,6 +4,7 @@ features across many structures.
 """
 
 from collections import Counter, defaultdict
+from statistics import mean
 from typing import Dict, Iterable
 
 from robocrys.stats.adapter import StatisticsAdapter
@@ -25,18 +26,57 @@ class StructureStatistics(object):
         self.stat_adapters = [StatisticsAdapter(cs, inc_oxi_state=inc_oxi_state)
                               for cs in condensed_structures]
 
+        self.element_counts = Counter()
+        for sa in self.stat_adapters:
+            self.element_counts += Counter(sa.species)
+
     def get_element_connectivity_probabilities(self):
-        element_counts = Counter()
         connectivity_counts = defaultdict(Counter)
 
         for sa in self.stat_adapters:
-            element_counts += Counter(set(sa.elements.values()))
             for element, counts in sa.get_element_connectivity_count(
                     normalize=True).items():
                 connectivity_counts[element] += Counter(counts)
 
-        def divide_counter(counter, denominator):
-            return {k: v/denominator for k, v in counter.items()}
-
-        return {el: divide_counter(counts, element_counts[el])
+        return {el: _divide_counter(counts, self.element_counts[el])
                 for el, counts in connectivity_counts.items()}
+
+    def get_element_dimensionality_probabilities(self):
+        dimensionality_counts = defaultdict(Counter)
+
+        for sa in self.stat_adapters:
+            counts = Counter([sa.dimensionality])
+            for element in sa.species:
+                dimensionality_counts[element] += counts
+
+        return {el: _divide_counter(counts, self.element_counts[el])
+                for el, counts in dimensionality_counts.items()}
+
+    def get_element_component_dimensionality_probabilities(self):
+        component_counts = Counter()
+        dimensionality_counts = defaultdict(Counter)
+
+        for sa in self.stat_adapters:
+            for component in sa.component_makeup:
+                counts = Counter([sa.components[component]['dimensionality']])
+                for element in sa.component_species[component]:
+                    dimensionality_counts[element] += counts
+
+                component_counts += Counter(sa.component_species[component])
+
+        return {el: _divide_counter(counts, component_counts[el])
+                for el, counts in dimensionality_counts.items()}
+
+    def get_element_corner_sharing_octahedral_tilt_angles(self):
+        angles = defaultdict(list)
+
+        for sa in self.stat_adapters:
+            el_angles = sa.get_element_corner_sharing_octahedra_tilt_angles()
+            for element, angle in el_angles.items():
+                angles[element].append(angle)
+
+        return {el: mean(angles) for el, angles in angles.items()}
+
+
+def _divide_counter(counter, denominator):
+    return {k: v/denominator for k, v in counter.items()}
