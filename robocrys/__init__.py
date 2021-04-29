a232e0bd248ca60ca217f07d5cc07b0053b1ffb2
Alex Ganose <alexganose@googlemail.com>
2021-04-29 11:49:48 -0700

    Fix init

--- robocrys/__init__.py
+++ robocrys/__init__.py
@@ -3,9 +3,9 @@ General imports.
 
 isort:skip_file
 """
-from robocrys._version import __version__
+from robocrys.util import common_formulas
 from robocrys.condense.condenser import StructureCondenser
 from robocrys.describe.describer import StructureDescriber
-from robocrys.util import common_formulas
+from robocrys._version import __version__
 
 __all__ = ["__version__", "StructureDescriber", "StructureCondenser", "common_formulas"]
